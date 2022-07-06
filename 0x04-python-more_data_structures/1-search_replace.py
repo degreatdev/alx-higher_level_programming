bf1363ca35162fd1c54d7d6c4e1dd5f362bf78b0
Osondu Dominion <domikdegreat@gmail.com>
2022-07-06 21:12:54 +0100

    Create 1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+def search_replace(my_list, search, replace):
+    new_1 = []
+    for x in my_list:
+        if x == search:
+            new_1.append(replace)
+        else:
+            new_1.append(x)
+    return new_1
