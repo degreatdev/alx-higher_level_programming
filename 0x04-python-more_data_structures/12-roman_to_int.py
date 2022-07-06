68b98969c50b7fb8543719e3d25e43f260d08fbe
Osondu Dominion <domikdegreat@gmail.com>
2022-07-06 21:48:49 +0100

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,33 +1,40 @@
 #!/usr/bin/python3
+def to_subtract(list_num):
+    sub = 0
+    max_num = max(list_num)
+
+    for n in list_num:
+        if max_num > n:
+            sub += n
+
+    return (max_num - sub)
+
 
 def roman_to_int(roman_string):
-
-    ns = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-
-    i = 0
-
-    total = 0
-
-    if isinstance(roman_string, str):
-
-        for i in range(len(roman_string) - 1):
-
-            if ns[roman_string[i]] >= ns[roman_string[i + 1]]:
-
-                total += ns[roman_string[i]]
-
-            else:
-
-                total -= ns[roman_string[i]]
-
-            i += 1
-
-        total += ns[roman_string[i]]
-
-        return total
-
-    else:
-
+    if not roman_string:
         return 0
-       
 
+    if not isinstance(roman_string, str):
+        return 0
+
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    key = list(roman.keys())
+
+    num = 0
+    last_rom = 0
+    list_num = [0]
+
+    for i in roman_string:
+        for j in key:
+            if j == i:
+                if roman.get(i) <= last_rom:
+                    num += to_subtract(list_num)
+                    list_num = [roman.get(i)]
+                else:
+                    list_num.append(roman.get(i))
+
+                last_rom = roman.get(i)
+
+    num += to_subtract(list_num)
+
+    return (num)
