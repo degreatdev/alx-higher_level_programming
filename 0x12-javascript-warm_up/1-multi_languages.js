ca9077938727d93df7ec872855592a25a073373f
Osondu Dominion <domikdegreat@gmail.com>
2022-10-05 04:05:46 -0700

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 
-console.log('C is fun'+'\n'+'Python is cool'+'\n'+'JavaScript is amazing');
-
+console.log('C is fun'+'\n');
+console.log('Python is cool'+'\n');
+console.log('JavaScript is amazing')
